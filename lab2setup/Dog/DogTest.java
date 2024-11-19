c624659deb51ba2d7a40c3a60e8da83514eb6552
satelesport <152187916+satelesport@users.noreply.github.com>
2024-11-19 16:30:07 +0800

    lab2 remark

--- lab2setup/Dog/DogTest.java
+++ lab2setup/Dog/DogTest.java
@@ -2,9 +2,10 @@ package Dog;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 
-public class DogTest {    
-    @Test
+public class DogTest {
+    @org.junit.jupiter.api.Test
     public void testSmall() {
         Dog d = new Dog(3);
         assertEquals("yip", d.noise());
