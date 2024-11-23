4b10c746557ec7ba0a6e5cfe296079844411b3cd
satelesport <152187916+satelesport@users.noreply.github.com>
2024-11-23 12:15:16 +0800

    Proj 1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -83,7 +83,7 @@ public class ArrayDeque<T> implements Iterable<T>, Deque<T> {
             return null;
         }
         if (size < array.length / 4 && size > 16) {
-            resize(array.length / 4 + 2);
+            resize(array.length / 4 + 1);
         }
         size -= 1;
 
@@ -97,7 +97,7 @@ public class ArrayDeque<T> implements Iterable<T>, Deque<T> {
             return null;
         }
         if (size < array.length / 4 && size > 16) {
-            resize(array.length / 4 + 2);
+            resize(array.length / 4 + 1);
         }
         size -= 1;
 
