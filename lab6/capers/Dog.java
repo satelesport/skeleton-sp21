0b654ae0308f3ca57840fe87e7ef60f96702d971
satelesport <152187916+satelesport@users.noreply.github.com>
2024-12-02 22:37:26 +0800

    Revert "Lab 6"
    This reverts commit e38d67a30be150d42a0149f35003ac9cb0d6fcf6.

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -58,7 +58,7 @@ public class Dog implements Serializable{ // TODO
      */
     public void saveDog() {
         File f = join(DOG_FOLDER, name);
-        writeContents(f,this);
+        Utils.writeContents(f,this);
     }
 
     @Override
