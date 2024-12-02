799817630434b0c9c250e7b300551146fe64a72b
satelesport <152187916+satelesport@users.noreply.github.com>
2024-12-03 00:02:53 +0800

    Lab 6 rewrite

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -18,8 +18,7 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = Utils.join(CWD,".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -31,7 +30,8 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CWD.mkdir();
+        CAPERS_FOLDER.mkdir();
     }
 
     /**
@@ -40,7 +40,16 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File f = Utils.join(CAPERS_FOLDER,"story");
+        if(f.exists()){
+            String newStory = Utils.readContentsAsString(f);
+            System.out.println(newStory);
+            Utils.writeContents(f, newStory + '\n' + text);
+        }
+        else{
+            System.out.println(text);
+            Utils.writeContents(f,text);
+        }
     }
 
     /**
@@ -49,7 +58,9 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog d = new Dog(name, breed, age);
+        d.saveDog();
+        d.toString();
     }
 
     /**
@@ -59,6 +70,7 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog d = Dog.fromFile(name);
+        d.haveBirthday();
     }
 }
