57d26370aa42463b262e80016b9ab434e85f0331
satelesport <152187916+satelesport@users.noreply.github.com>
2024-12-03 23:43:48 +0800

    Proj 2
    list all the commands in the Main.java, need to implement them in Respository.java

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -3,13 +3,18 @@ package gitlet;
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author TODO
  */
+import gitlet.Repository;
+
 public class Main {
 
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        if(args.length == 0){  //when nothing is read
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
@@ -18,7 +23,42 @@ public class Main {
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
-            // TODO: FILL THE REST IN
+            case "comment":
+
+                break;
+            case "rm":
+
+                break;
+            case "log":
+
+                break;
+            case "global-log":
+
+                break;
+            case "find":
+
+                break;
+            case "status":
+
+                break;
+            case "checkout":
+
+                break;
+            case "branch":
+
+                break;
+            case "rm-branch":
+
+                break;
+            case "reset":
+
+                break;
+            case "merge":
+
+                break;
+            default:
+                System.out.println("No command with that name exists.");
+                System.exit(0);
         }
     }
 }
