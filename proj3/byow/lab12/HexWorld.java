30d9fe7dc1ebe6ed195b5101b2bf47857f7a5a76
satelesport <152187916+satelesport@users.noreply.github.com>
2024-12-12 14:31:55 +0800

    lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,45 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 30;
+
+    public static void addHexagon(TETile[][] tiles, int size, int x, int y){
+        for(int i = 0; i < size; i++){
+//            for(int j = 0; j < size - i + 1; j++){
+//                tiles[x + j][y + i] = Tileset.NOTHING;
+//            }
+            for(int k = 1; k <= size + i * 2; k++){
+                tiles[x + size - 1 - i + k][y + i] = Tileset.FLOWER;
+            }
+        }
+
+        for(int i = size - 1; i >= 0; i--){
+//            for(int j = 0; j < size - i + 1; j++){
+//                tiles[x + 2 * size - 1 - i][y + j] = Tileset.NOTHING;
+//            }
+            for(int k = 1; k <= size + i * 2 ; k++){
+                tiles[x + size - 1 - i + k][y + 2 * size - 1 - i] = Tileset.FLOWER;
+            }
+        }
+    }
+
+    public static void fillWithNoting(TETile[][] tiles){
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
+        fillWithNoting(randomTiles);
+        addHexagon(randomTiles, 3, 10, 10);
+        ter.renderFrame(randomTiles);
+    }
 }
